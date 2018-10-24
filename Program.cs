33a952516490abdcee9ff6bed00b7ab3aaf82d5c
Kyle Kamperschroer <kykamper@microsoft.com>
2018-10-24 09:19:37 -0700

    Initial hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureNHDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
