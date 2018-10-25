9f91f4886ab0dafdf9663ec9fcf17216e4f4d71d
Denis Stankovski <destanko@microsoft.com>
2018-10-25 13:34:45 -0700

    Added sample for creating/deleting registrations

--- Samples/RegistrationSample/SampleConfiguration.cs
+++ Samples/RegistrationSample/SampleConfiguration.cs
@@ -2,9 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for
 // license information.
 
-using System;
-
-namespace SendPushSample
+namespace RegistrationSample
 {
     public class SampleConfiguration
     {
