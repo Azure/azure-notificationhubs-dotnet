ae35b8d2bd95736017e644426b50fcda0e8222f6
Shantnu Gehlot <shantnug@microsoft.com>
2018-10-25 12:24:45 -0700

    Registeation sample

---
+++ Samples/RegistrationSample/Program.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.Extensions.Configuration;
+
+namespace SendPushSample
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+           
+        }
+
+    }        
+}
