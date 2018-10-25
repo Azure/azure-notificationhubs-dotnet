931dfe35d679b5bb75f5bbf8216555a266cdcf92
Kyle Kamperschroer <kykamper@microsoft.com>
2018-10-25 16:54:29 -0700

    Inject Package.StoreAssociation.xml at build time

--- Samples/appcenter-post-clone.ps1
+++ Samples/appcenter-post-clone.ps1
@@ -1,3 +1,6 @@
-$secretsPath = ".\UwpSample\~Secrets.cs";
+$secretsPath = ".\UwpSample\~Secrets.cs"
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_NAME>',$env:HUB_NAME) | Set-Content -Path $secretsPath
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_CONNECTION_STRING>',$env:HUB_CONNECTION_STRING) | Set-Content -Path $secretsPath
+
+$packageStoreAssociationXmlPath = ".\UwpSample\Package.StoreAssociation.xml"
+Write-Output $env:PACKAGE_STORE_ASSOCIATION_CONTENT | Set-Content -Path $packageStoreAssociationXmlPath
