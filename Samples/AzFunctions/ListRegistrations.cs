3217da92dfe722bd43343f372d9cb76f7c4a6bb8
Karl Piteira <kpiteira@hotmail.com>
2018-10-25 12:18:05 -0700

    Added function

---
+++ Samples/AzFunctions/ListRegistrations.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.NotificationHubs;
+
+namespace nh_samples_function
+{
+    public static class ListRegistrations
+    {
+        [FunctionName("ListRegistrations")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string hubName = req.Query["hubName"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            hubName = hubName ?? data?.hubName;
+            if(hubName == null){
+                return new BadRequestObjectResult("Please pass a Hub name on the query string or in the request body");
+            }
+
+            var hub = NotificationHubClient.CreateClientFromConnectionString(
+                NHubSamples.Parameters.QueryString, 
+                hubName);
+
+            CollectionQueryResult<RegistrationDescription> regs = await hub.GetAllRegistrationsAsync(0);
+            string result = "";int quantity = 0;
+            foreach(RegistrationDescription reg in regs){
+                result += $"{reg.RegistrationId}";
+                if(reg.Tags != null){
+                    result += $"-> {reg.Tags.Count} tags: {reg.Tags.ToString()}";
+                } else{
+                    result += "-> No tags";
+                }
+                result += "\n";
+                quantity++;
+            }
+            return (ActionResult) new OkObjectResult($"There are {quantity} registrations:\n{result}");
+        }
+    }
+}
