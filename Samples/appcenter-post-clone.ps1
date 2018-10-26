1ea31a8a870cab1e06379e885cad680893984bd9
Kyle Kamperschroer <kykamper@microsoft.com>
2018-10-25 17:11:08 -0700

    Only inject customizations to Package.StoreAssociation at runtime

--- Samples/appcenter-post-clone.ps1
+++ Samples/appcenter-post-clone.ps1
@@ -2,5 +2,12 @@ $secretsPath = ".\UwpSample\~Secrets.cs"
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_NAME>',$env:HUB_NAME) | Set-Content -Path $secretsPath
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_CONNECTION_STRING>',$env:HUB_CONNECTION_STRING) | Set-Content -Path $secretsPath
 
+$packageStoreAssociationXmlTemplatePath = ".\UwpSample\Package.StoreAssociation.xml.template"
 $packageStoreAssociationXmlPath = ".\UwpSample\Package.StoreAssociation.xml"
-Write-Output $env:PACKAGE_STORE_ASSOCIATION_CONTENT | Set-Content -Path $packageStoreAssociationXmlPath
+((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PUBLISHER__',$env:STORE_PUBLISHER) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
+((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PUBLISHER_DISPLAY_NAME__',$env:STORE_PUBLISHER_DISPLAY_NAME) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
+((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PACKAGE_IDENTITY_NAME__',$env:STORE_PACKAGE_IDENTITY_NAME) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
+((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PACKAGE_RESERVED_NAME__',$env:STORE_PACKAGE_RESERVED_NAME) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
+((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_APP_ID__',$env:STORE_APP_ID) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
+
+Get-Content $packageStoreAssociationXmlTemplatePath | Set-Content -Path $packageStoreAssociationXmlPath
