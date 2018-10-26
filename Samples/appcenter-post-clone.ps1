e716c172ad63daea6a2fd84cfd40ff03b78a8656
Kyle Kamperschroer <kykamper@microsoft.com>
2018-10-25 17:22:54 -0700

    Make post-clone script more flexible about working directory

--- Samples/appcenter-post-clone.ps1
+++ Samples/appcenter-post-clone.ps1
@@ -1,9 +1,9 @@
-$secretsPath = ".\UwpSample\~Secrets.cs"
+$secretsPath = Join-Path -Path $PSScriptRoot -ChildPath ".\UwpSample\~Secrets.cs"
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_NAME>',$env:HUB_NAME) | Set-Content -Path $secretsPath
 ((Get-Content -path $secretsPath -Raw) -replace '<HUB_CONNECTION_STRING>',$env:HUB_CONNECTION_STRING) | Set-Content -Path $secretsPath
 
-$packageStoreAssociationXmlTemplatePath = ".\UwpSample\Package.StoreAssociation.xml.template"
-$packageStoreAssociationXmlPath = ".\UwpSample\Package.StoreAssociation.xml"
+$packageStoreAssociationXmlTemplatePath = Join-Path $PSScriptRoot -ChildPath ".\UwpSample\Package.StoreAssociation.xml.template"
+$packageStoreAssociationXmlPath = Join-Path $PSScriptRoot -ChildPath ".\UwpSample\Package.StoreAssociation.xml"
 ((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PUBLISHER__',$env:STORE_PUBLISHER) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
 ((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PUBLISHER_DISPLAY_NAME__',$env:STORE_PUBLISHER_DISPLAY_NAME) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
 ((Get-Content -path $packageStoreAssociationXmlTemplatePath -Raw) -replace '__STORE_PACKAGE_IDENTITY_NAME__',$env:STORE_PACKAGE_IDENTITY_NAME) | Set-Content -Path $packageStoreAssociationXmlTemplatePath
