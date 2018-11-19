11c349b67cf878d95e0c6a56b4a69e54e5cb1034
Denis Stankovski <denis.stankovski@gmail.com>
2018-11-19 15:39:31 +0000

    Update src/Microsoft.Azure.NotificationHubs/AndroidTemplateRegistrationDescription.cs
    Co-Authored-By: iTOYS <toysruss89@gmail.com>

--- src/Microsoft.Azure.NotificationHubs/AndroidTemplateRegistrationDescription.cs
+++ src/Microsoft.Azure.NotificationHubs/AndroidTemplateRegistrationDescription.cs
@@ -205,7 +205,7 @@ namespace Microsoft.Azure.NotificationHubs
         /// <summary>
         /// Creates instance of <see cref="T:Microsoft.Azure.NotificationHubs.FcmTemplateRegistrationDescription"/> class using given Firebase Cloud Messaging registration id
         /// </summary>
-        /// <param name="fcmRegistrationId">Registration id obtained from the Google Cloud Messaging service</param>
+        /// <param name="fcmRegistrationId">Registration id obtained from the Firebase Cloud Messaging service</param>
         public FcmTemplateRegistrationDescription(string fcmRegistrationId)
             : base(fcmRegistrationId)
         {
