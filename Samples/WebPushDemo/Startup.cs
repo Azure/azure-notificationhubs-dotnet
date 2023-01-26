d6bd0b4bc1dbf22f071880eeee827cf92c61d5f5
Ghania M <113198670+ghmous-github@users.noreply.github.com>
2023-01-26 11:10:58 -0800

    Add files via upload

---
+++ Samples/WebPushDemo/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebPushDemo.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace WebPushDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            
+            services.AddDbContext<WebPushDemoContext>(options =>
+                options.UseSqlite("Data Source=Data/WebPushDb.db"));
+
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            if (Configuration.GetSection("VapidKeys")["PublicKey"].Length == 0 || Configuration.GetSection("VapidKeys")["PrivateKey"].Length == 0)
+            {
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute("default", "{controller=WebPush}/{action=GenerateKeys}/{id?}");
+                });
+
+                return;
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Devices}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
