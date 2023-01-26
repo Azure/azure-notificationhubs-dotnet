2c908a260b32db90e991ad49bebf70348ecc047e
Ghania M <113198670+ghmous-github@users.noreply.github.com>
2023-01-26 11:27:17 -0800

    Add files via upload

---
+++ Samples/WebPushDemo/Controllers/DevicesController.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebPushDemo.Models;
+
+namespace WebPushDemo.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private readonly WebPushDemoContext _context;
+
+        private readonly IConfiguration _configuration;
+
+        public DevicesController(WebPushDemoContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: Devices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Devices.ToListAsync());
+        }
+
+        // GET: Devices/Create
+        public IActionResult Create()
+        {
+            ViewBag.PublicKey = _configuration.GetSection("VapidKeys")["PublicKey"];
+
+            return View();
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PushEndpoint,PushP256DH,PushAuth")] Devices devices)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(devices);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(devices);
+        }
+       
+        // GET: Devices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var devices = await _context.Devices
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (devices == null)
+            {
+                return NotFound();
+            }
+
+            return View(devices);
+        }
+
+        // POST: Devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var devices = await _context.Devices.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Devices.Remove(devices);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Samples/WebPushDemo/Controllers/WebPushController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebPush;
+using WebPushDemo.Models;
+
+namespace WebPushDemo.Controllers
+{
+    public class WebPushController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly WebPushDemoContext _context;
+
+        public WebPushController(WebPushDemoContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public IActionResult Send(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Send")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Send(int id)
+        {
+            var payload = Request.Form["payload"];
+            var device = await _context.Devices.SingleOrDefaultAsync(m => m.Id == id);
+
+            string vapidPublicKey = _configuration.GetSection("VapidKeys")["PublicKey"];
+            string vapidPrivateKey = _configuration.GetSection("VapidKeys")["PrivateKey"];
+
+            var pushSubscription = new PushSubscription(device.PushEndpoint, device.PushP256DH, device.PushAuth);
+            var vapidDetails = new VapidDetails("mailto:example@example.com", vapidPublicKey, vapidPrivateKey);
+
+            var webPushClient = new WebPushClient();
+            webPushClient.SendNotification(pushSubscription, payload, vapidDetails);
+
+            return View();
+        }
+
+        public IActionResult GenerateKeys()
+        {
+            var keys = VapidHelper.GenerateVapidKeys();
+            ViewBag.PublicKey = keys.PublicKey;
+            ViewBag.PrivateKey = keys.PrivateKey;
+            return View();
+        }
+    }
+}
